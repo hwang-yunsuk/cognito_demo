74d4ef996a448d686028312c5667f5ff6087ae99
hwang <cararito@naver.com>
2024-07-17 02:49:51 +0900

    token 設定

--- src/main/java/cognito/example/cognito_demo/config/CustomOAuth2LoginSuccessHandler.java
+++ src/main/java/cognito/example/cognito_demo/config/CustomOAuth2LoginSuccessHandler.java
@@ -1,11 +1,10 @@
 package cognito.example.cognito_demo.config;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequestEntityConverter;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -14,16 +13,14 @@ import java.io.IOException;
 @Component
 public class CustomOAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
-    private final DefaultAuthorizationCodeTokenResponseClient tokenResponseClient;
-
-    public CustomOAuth2LoginSuccessHandler() {
-        tokenResponseClient = new DefaultAuthorizationCodeTokenResponseClient();
-        OAuth2AuthorizationCodeGrantRequestEntityConverter requestEntityConverter = new OAuth2AuthorizationCodeGrantRequestEntityConverter();
-        tokenResponseClient.setRequestEntityConverter(requestEntityConverter);
-    }
-
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        OidcUser oidcUser = (OidcUser) oauthToken.getPrincipal();
+        String idToken = oidcUser.getIdToken().getTokenValue();
+
+        request.getSession().setAttribute("idToken", idToken);
+
         response.sendRedirect("/siteA");
     }
 }
--- src/main/java/cognito/example/cognito_demo/controller/BasicController.java
+++ src/main/java/cognito/example/cognito_demo/controller/BasicController.java
@@ -2,7 +2,10 @@ package cognito.example.cognito_demo.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class BasicController {
@@ -14,13 +17,15 @@ public class BasicController {
         return "login";
     }
 
-    @GetMapping("/siteA")
-    public String siteA() {
-        return "siteA";
-    }
+    // @GetMapping("/siteA")
+    // public String siteA() {
+    //     return "siteA";
+    // }
 
-    @GetMapping("/siteB")
-    public String siteB() {
-        return "siteB";
+    @GetMapping("/siteA")
+    public String home(HttpServletRequest request, Model model) {
+        String idToken = (String) request.getSession().getAttribute("idToken");
+        model.addAttribute("token", idToken);
+        return "siteA";  // siteA.htmlに対応するテンプレート
     }
 }
