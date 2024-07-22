fd924d2eb0ed85f31a91b12d3c148418e3ea761a
hwang <cararito@naver.com>
2024-07-23 01:38:50 +0900

    cognitoService修正

--- src/main/java/cognito/example/cognito_demo/service/CognitoService.java
+++ src/main/java/cognito/example/cognito_demo/service/CognitoService.java
@@ -1,5 +1,6 @@
 package cognito.example.cognito_demo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
@@ -14,23 +15,30 @@ import java.util.List;
 @Service
 public class CognitoService {
 
-    private static final String USER_POOL_ID = "us-east-1_kPfzTh4dA"; // ユーザープールID
-    private static final String AKI = "";
-    private static final String SAK = "";
-    private static final Region REGION = Region.US_EAST_1; // リージョン
+    @Value("${aws.cognito.userPoolId}")
+    private String userPoolId;
+
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretAccessKey}")
+    private String secretAccessKey;
+
+    @Value("${aws.region}")
+    private String region;
 
     private CognitoIdentityProviderClient createCognitoClient() {
-        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(AKI, SAK);
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
         return CognitoIdentityProviderClient.builder()
                 .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
-                .region(REGION)
+                .region(Region.of(region))
                 .build();
     }
 
     public boolean checkIfUserExists(String email) {
         try (CognitoIdentityProviderClient cognitoClient = createCognitoClient()) {
             ListUsersRequest request = ListUsersRequest.builder()
-                    .userPoolId(USER_POOL_ID)
+                    .userPoolId(userPoolId)
                     .filter("email = \"" + email + "\"")
                     .build();
 
